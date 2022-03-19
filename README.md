947c9648b188401d9a68d9ee3ca860a0752b69d7
Douglas S. Green <douglas@nurd.site>
2022-03-19 19:26:31 -0400

    Adding the pig dice game

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ Here are a few simple text games written for SWI Prolog.
 
 To run a game, load it in Prolog and type: `play.`
 
+## Dice
+
+The dice games are in the dice subdirectory. So far, there's a game of
+[Pig](https://en.wikipedia.org/wiki/Pig_(dice_game)).
+
 ## Hangman
 
-Dictionary definitions of words are provided from Wiktionary. Guess the word in six tries or less.
+Dictionary definitions of words are provided from [Wiktionary](https://en.wiktionary.org/wiki/Wiktionary:Main_Page).
+Guess the word in six tries or less.
 
 ## Quiz
 
---
+++ dice/pig.pl
@@ -0,0 +1,108 @@
+/** <module> Play the pig game of dice.
+
+Pig is a dice game where the object is to roll more than 100 points. A player loses the round if they roll a 1, called
+a pig.
+
+@author Douglas S. Green
+@license GPL
+*/
+module(quiz, [play/0]).
+
+%! play.
+play :-
+    format("Type R to roll, H to hold, Q to quit.~n"),
+    human(0, 0, 0).
+
+%! human(HumanScore:int, ComputerScore:int, Round:int)
+% It's the human's turn to play with the given total scores and the round score.
+human(HumanScore, ComputerScore, Round) :-
+    get_single_char(Code),
+    char_code(Char, Code),
+    downcase_atom(Char, Low),
+    (
+        Low = 'r',
+        roll("You", Die, Round, NewRound),
+        (
+            Die = 1,
+            computer(HumanScore, ComputerScore, 0);
+            Die \= 1,
+            human(HumanScore, ComputerScore, NewRound)
+        ),
+        !;
+        Low = 'h',
+        NewHumanScore is HumanScore + Round,
+        format("Your score is ~d.\n", [NewHumanScore]),
+        computer(NewHumanScore, ComputerScore, 0),
+        !;
+        Low = 'q',
+        !;
+        human(HumanScore, ComputerScore, Round)
+    ).
+
+%! computer(HumanScore:int, ComputerScore:int, Round:int)
+% It's the computer's turn to play with the given total scores and the round score.
+computer(HumanScore, ComputerScore, Round) :-
+    (
+        % The computer must always roll during the first round.
+        Round = 0,
+        ShouldRoll = true,
+        !;
+        % If the human is winning in the last round, the computer must keep on rolling.
+        HumanScore >= 100,
+        ComputerScore =< HumanScore,
+        ShouldRoll = true,
+        !;
+        % If the computer is winning in the last round, the computer must stop rolling.
+        HumanScore >= 100,
+        ComputerScore > HumanScore,
+        ShouldRoll = false,
+        !;
+        % Otherwise the computer should keep rolling 3/4 of the time.
+        random_between(1, 4, R),
+        (
+            R > 3,
+            ShouldRoll = true;
+            R =< 3,
+            ShouldRoll = false
+        )
+    ),
+    (
+        ShouldRoll,
+        roll("The computer", Die, Round, NewRound),
+        (
+            Die = 1,
+            (
+                % Because the computer must keep on rolling when it's losing in the last round, it can only lose on a
+                % pig.
+                HumanScore >= 100,
+                HumanScore >= ComputerScore,
+                format("You win with a score of ~d to ~d!\n", [HumanScore, ComputerScore]);
+                human(HumanScore, ComputerScore, 0)
+            );
+            Die \= 1,
+            computer(HumanScore, ComputerScore, NewRound)
+        );
+        \+ ShouldRoll,
+        NewComputerScore is ComputerScore + Round,
+        format("The computer's score is ~d.\n", [NewComputerScore]),
+        (
+            NewComputerScore >= 100,
+            NewComputerScore > HumanScore,
+            format("The computer wins with a score of ~d to ~d!\n", [NewComputerScore, HumanScore]),
+            !;
+            human(HumanScore, NewComputerScore, 0)
+        )
+    ).
+
+%! roll(Player:string, Die:int, Round:int, NewRound:int)
+% The player rolls a die yielding a new round score. 
+roll(Player, Die, Round, NewRound) :-
+    random_between(1, 6, Die),
+    (
+        Die = 1,
+        format("~w rolled a pig!\n", [Player]),
+        NewRound = 0;
+        Die \= 1,
+        NewRound is Round + Die,
+        format("~w rolled a ~d for a total of ~d.\n", [Player, Die, NewRound])
+    ).
